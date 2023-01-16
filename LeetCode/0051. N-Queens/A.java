f405dd0a2d1fcc146188c1446e3afaaeb1d7f924
allaboutevemirolive <112833599+allaboutevemirolive@users.noreply.github.com>
2023-01-17 00:59:38 +0200

    update

--- LeetCode/0051. N-Queens/A.java
+++ LeetCode/0051. N-Queens/A.java
@@ -3,7 +3,7 @@ import java.util.List;
 class A {
     public static void main(String[] args) {
         C C = new C();
-        int n = 8; // Example value, replace with desired input
+        int n = 4; // Example value, replace with desired input
         List<List<String>> ans = C.solveNQueens(n);
         for (List<String> list : ans) {
             for (String s : list) {
