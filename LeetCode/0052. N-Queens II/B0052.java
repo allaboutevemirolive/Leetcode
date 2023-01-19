591986d6af3b2a0a7ae724fe94fc62d5090f7938
allaboutevemirolive <112833599+allaboutevemirolive@users.noreply.github.com>
2023-01-19 22:13:37 +0200

    update

--- LeetCode/0052. N-Queens II/B0052.java
+++ LeetCode/0052. N-Queens II/B0052.java
@@ -1,20 +1,30 @@
 class B0052 {
-    int count;
+    // variable to keep track of number of solutions found
+    int count; 
     public int totalNQueens(int n) {
-        count = 0;
-        backtrack(0, 0, 0, 0, n);
-        return count;
+        // initialize count to 0
+        count = 0; 
+        // call backtrack function with initial bitmasks
+        backtrack(0, 0, 0, 0, n); 
+        // return the number of solutions found
+        return count; 
     }
     private void backtrack(int row, int ld, int col, int rd, int n) {
-        if (row == (1 << n) - 1) {
-            count++;
+        // if all rows are filled
+        if (row == (1 << n) - 1) { 
+            // increment count, a solution is found
+            count++; 
             return;
         }
-        int pos = (~(row | ld | col | rd)) & ((1 << n) - 1);
+        // generate bitmask for empty positions
+        int pos = (~(row | ld | col | rd)) & ((1 << n) - 1); 
         while (pos > 0) {
-            int p = pos & -pos;
-            pos -= p;
-            backtrack((row | p), (ld | p) << 1, (col | p), (rd | p) >> 1, n);
+            // get lowest set bit in pos
+            int p = pos & -pos; 
+            // mark this position as filled
+            pos -= p; 
+            // recursive call with updated bitmasks
+            backtrack((row | p), (ld | p) << 1, (col | p), (rd | p) >> 1, n); 
         }
     }
 }
