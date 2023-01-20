3a2b1a8f086638d654e51a2029b8ac8409958c92
allaboutevemirolive <112833599+allaboutevemirolive@users.noreply.github.com>
2023-01-20 16:22:03 +0200

    Update

--- LeetCode/0055. Jump Game/A0055.java
+++ LeetCode/0055. Jump Game/A0055.java
@@ -2,7 +2,7 @@ public class A0055 {
 
     public static void main(String[] args) {
         B0055 b0055 = new B0055();
-        int[] nums = { 3, 2, 1, 0, 4 };
+        int[] nums = { 2, 3, 1, 1, 4 };
         boolean result = b0055.canJump(nums);
         System.out.println(result);
     }
--- LeetCode/0055. Jump Game/B0055.java
+++ LeetCode/0055. Jump Game/B0055.java
@@ -1,12 +1,21 @@
 public class B0055 {
-    
+    /**
+     * This method checks if it is possible to reach the last index of an input array by jumping from index to index.
+     * @param nums the input array of integers
+     * @return true if it is possible to reach the last index, false otherwise
+     */
     public boolean canJump(int[] nums) {
+        // Initialize lastPos as the last index of the input array
         int lastPos = nums.length - 1;
+        // Iterate through the array in reverse order
         for (int i = nums.length - 2; i >= 0; i--) {
+            // Check if the current index plus its value is greater or equal to the lastPos
             if (i + nums[i] >= lastPos) {
+                // If true, update lastPos as the current index
                 lastPos = i;
             }
         }
+        // Check if the lastPos is the first index of the array
         return lastPos == 0;
     }
 }
